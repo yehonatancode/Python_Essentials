dca2f4b18566317edfb9a0535ef79eb8ac2d2d68
yehonatancode <yehonatantseva@gmail.com>
2021-11-22 20:13:45 +0200

    Articles minor changes

--- WebScraping/BBCScraper.py
+++ WebScraping/BBCScraper.py
@@ -4,9 +4,9 @@ import requests
 import time
 
 
-urls = []
-all_headlines = []
-articles = []
+total_urls = []
+total_headlines = []
+total_articles = []
 url_as_key ={} #saving articles as value, url as key
 
 response = requests.get('http://www.bbc.co.uk/news')
@@ -15,12 +15,13 @@ doc = BeautifulSoup(response.text, 'html.parser')
 
 def get_headlines():
     headlines = doc.find_all('h3')
+    all_headlines = []
 
     for headline in headlines:
         print(headline.text)
         all_headlines.append(headline.text)
         time.sleep(0.1)
-
+        return all_headlines
 
 #Using the ['href] attribute we recieve the url, without the website(bbc.com,google.com,etc. to fill this gap, I added this func.
 def create_url(website,link):
@@ -34,7 +35,8 @@ def create_url(website,link):
 #note: there's a limited amount of web entries before BBC blocks login attempt. Adding sleep to try prevent block.
 #the function raises an exception. hence, we'll use a try/except/finally approach
 def get_content():
-
+    urls = []
+    articles = []
     website = "http://bbc.com"
     links = doc.find_all('a', { 'class': 'gs-c-promo-heading' })
     for link in links:
@@ -49,19 +51,24 @@ def get_content():
             time.sleep(1)  # waiting x amount of seconds to prevent blocking
     except Exception as e:
         print("Connection error,", e.__class__, "occurred.")
-    finally:
-        url_to_articles()
-        print(url_as_key)
+    return urls, articles
+
+
+
 
 
 
 
 def url_to_articles():
-    for i in range(len(urls)):
-        url_as_key[urls[i]] = articles[i]
+    for i in range(len(total_urls)):
+        url_as_key[total_urls[i]] = total_articles[i]
 
 
-get_content()
+total_headlines = get_headlines()
+print(total_headlines)
+total_urls, total_articles = get_content()
+print(total_urls)
+print(total_articles)
 
 
 
