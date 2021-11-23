286661c249b3c7fd0fe1af4782e72254a0b02953
yehonatancode <yehonatantseva@gmail.com>
2021-11-23 19:41:32 +0200

    Removed unnecessary files

---
+++ WebScraping/FlightsScraper.py
@@ -0,0 +1,2 @@
+#beautifulsoup is not suitable for this kind of webpage, as it is Java-Script based. Hence, we are only getting
+#the raw data, JavaScript isn't running, and the flights table is relied on JavaScript.
